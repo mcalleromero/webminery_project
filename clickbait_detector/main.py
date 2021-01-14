b81dd99a4678595c9fd1dec5ee5716cb3e987a68
Javier Aróstegui <javier.arostegui@estudiante.uam.es>
2021-01-14 18:18:24 +0100

    models working

--- clickbait_detector/main.py
+++ clickbait_detector/main.py
@@ -23,16 +23,6 @@ if __name__ == "__main__":
     filename = f'title_dataset_{today}.csv'
     final_file_path = DATA / filename
     data = pd.read_csv(final_file_path)
-    
-    #MODELS    
-    #DUMMY CLASSIFIER
-    #clf = DummyClassifier(strategy='most_frequent')
-    #RANDOM FOREST
-    #clf = RandomForestClassifier()
-    #NAIVE BAYES
-    #clf = MultinomialNB()
-    #LOGISTIC REGRESION
-
 
     df = data.sample(frac=1).reset_index(drop=True)
 
@@ -43,8 +33,6 @@ if __name__ == "__main__":
     y_train = y.head(int(len(df)*0.66))
     X_test = X.tail(int(len(df)*0.33))
     y_test = y.tail(int(len(df)*0.33))
-
-    #skf = StratifiedKFold(n_splits=5)
 
     auc_roc = []
 
@@ -60,14 +48,6 @@ if __name__ == "__main__":
         preds_proba = clf.predict_proba(X_test)
         preds = clf.predict(X_test)
 
-        # for train_index, test_index in skf.split(X, y):
-        #     X_train, X_test = X.loc[train_index], X.loc[test_index]
-        #     y_train, y_test = y.loc[train_index], y.loc[test_index]
-
-        #     # Hay que probar que funcione porque no me fio de nuestra capacidad de hacer que las cosas funcionen
-        #     classifier.fit(X_train, y_train, clf)
-        #     preds = classifier.predict_proba(X_test)
-
         auc = metrics.roc_auc_score(y_test, preds_proba[:,1])
         accuracy = metrics.accuracy_score(y_test, preds)
         precision = metrics.precision_score(y_test, preds)
@@ -80,6 +60,3 @@ if __name__ == "__main__":
     
     paramsFile.close()
     metricsFile.close()
-
-    # print(auc_roc)
-    # print(f'ROC_AUC_mean: {np.mean(auc_roc)}')
